4e6e2836bdf3b593f195b590cb72aa4d14d2c909
sergey@telminov.ru <sergey@telminov.ru>
2017-08-03 09:21:50 +0300

    увеличил версию пакета

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sw-python-email-devino',
-    version='0.0.1',
+    version='0.0.2',
     description='Integration with email API of devinotele.com',
     author='Telminov Sergey',
     url='https://github.com/telminov/sw-python-email-devino',
